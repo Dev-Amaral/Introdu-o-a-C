f3f00bde6899a53b4bdbd8201c36028f1115c68f
Davi Amaral <158376236+Dev-Amaral@users.noreply.github.com>
2024-03-27 13:41:35 -0300

    Update dividendo-divisor-quociente-resto.c

--- dividendo-divisor-quociente-resto.c
+++ dividendo-divisor-quociente-resto.c
@@ -6,7 +6,7 @@ int main (){
 	scanf("%d",&dividendo);
 	printf("Digite o divisor\n");
 	scanf("%d",&divisor);
-	printf("Quociente= %d\n",dividendo/divisor);
+	printf("Quociente= %d\n",dividendo/divisor); // o resultado sairá inteiro por causa da formatação %d
 	printf("Resto da divisao= %d\n", dividendo%divisor);
 	
 	return 0;
